1c7fdc00326ba669ae111121fb8a5b3080b85da0
Antal Perger <antal.perger@sap.com>
2024-03-16 11:57:15 +0000

    BAS preview

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -12,38 +12,24 @@ sap.ui.define([
 
             },
             callJoke() {
-                var appId = this.getOwnerComponent().getManifestEntry("/sap.app/id");
-                console.log("appId: " + appId);
-
-                var appPath = appId.replaceAll(".", "/");
-                console.log("appPath: " + appPath);
-
-                // var appModulePath = jQuery.sap.getModulePath(appPath);
-                var appModulePath = sap.ui.require.toUrl(appPath);
-
-                console.log("appModulePath: " + appModulePath)
-
                 $.get("./chuckdest/jokes/random", function(data) {
                     console.log(data);
                     alert(data.value);
                 });
-
-            //     const settings = {
-            //         "async": true,
-            //         "crossDomain": true,
-            //         "url": "/jokes/random",
-            //         "method": "GET",
-            //         "headers": {
-            //             "user-agent": "vscode-restclient"
-            //         }
-            //     };
-
-            //     $.ajax(settings).done(function (response) {
-            //         console.log(response);
-            //         alert(response.value);
-            //     });
+             },
+             callNorth() {
+                $.get("./north/V3/OData/OData.svc/", function(data) {
+                    console.log(data);
+                    var xmlSerializer = new XMLSerializer();
+                    var xmlStr = xmlSerializer.serializeToString(data);
+                    alert(xmlStr);
+                });
+             },
+             callNorthDirect() {
+                $.get("https://services.odata.org/V3/OData/OData.svc/", function(data) {
+                    console.log(data);
+                });
              }
-
 
         });
     });
